6b5df1801ecfc3e7a6ab719a831b66370685a235
Mahi-Goyal / <maahigoyal32@gamil.com>
2024-11-25 01:13:29 +0530

    updated

--- Sorting/SelectionSort.c
+++ Sorting/SelectionSort.c
@@ -6,6 +6,7 @@ int main()
     scanf("%d", &n);
     int arr[n];
     printf("Enter %d values for the array:\n", n);
+    
     for (int i = 0; i < n; i++) 
     {
         printf("Value %d: ", i + 1);
